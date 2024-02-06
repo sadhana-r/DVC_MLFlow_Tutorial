c8cac4bae59d24105307d5c279569651f3301e0a
sadhanaravikumar <sadhana.ravikumar@kitware.com>
2024-02-06 07:40:09 -0500

    mlflow

--- src/pipeline/train_mlflow.py
+++ src/pipeline/train_mlflow.py
@@ -7,33 +7,38 @@ from lightning.pytorch.callbacks import ModelCheckpoint
 from lightning.pytorch.trainer.trainer import Trainer
 from model import Net
 from datamodule import LungSegmentationDataModule
-import config
+import argparse
 
-def setup_mlflow_logger():
+parser = argparse.ArgumentParser()
+parser.add_argument('--params', '-p', type = str, help = 'params file', required = True)
 
-    mlflow.set_tracking_uri(config.mlflow_uri)
-    mlflow.set_experiment(config.experiment_name)
-    mlflow.set_experiment_tags(config.experiment_tags)
+
+def setup_mlflow_logger(params):
+
+    mlflow.set_tracking_uri(params["mlflow"]["mlflow_uri"])
+    mlflow.set_experiment(params["mlflow"]["experiment_name"])
+    mlflow.set_experiment_tags(params["mlflow"]["experiment_tags"])
 
     # Using a combination of direct mlflow logging and pytorch lightning logger
-    mlflow.pytorch.autolog()
+    mlflow.pytorch.autolog(log_datasets = True)
 
 def main():
 
-    params = yaml.safe_load(open("params.yaml"))
+    args = parser.parse_args()
 
-    setup_mlflow_logger()
+    params = yaml.safe_load(open(args.params))
 
-    mlflow.start_run(run_name=config.run_name)
+    setup_mlflow_logger(params)
+
+    mlflow.start_run(run_name=params["mlflow"]["run_name"])
 
     mlf_logger = MLFlowLogger(experiment_name=mlflow.get_experiment(mlflow.active_run().info.experiment_id).name,
         tracking_uri=mlflow.get_tracking_uri(),
         run_id=mlflow.active_run().info.run_id)
-    #mlflow.log_params(config)
 
     # data module
     dm = LungSegmentationDataModule(os.path.join(params["dataset"]["data_dir"],'datalist.csv'),batch_size= params["training_parameter"]["batch_size"],
-                                    input_size = params["network_parameter"]["input_size"], num_workers=params["training_parameter"]["num_workers"])
+                                    input_size = params["network_parameter"]["input_size"], num_workers=params["dataset"]["num_workers"])
 
     # Network
     model = Net(input_size=params["network_parameter"]["input_size"],
@@ -43,7 +48,7 @@ def main():
     # Trainer
     checkpoint_callback = ModelCheckpoint(dirpath="../lightning_models/", save_top_k=1, monitor="val_loss", filename='{run_name}_lungseg-{epoch:02d}-{val_loss:.2f}')
 
-    trainer = Trainer(min_epochs=0, max_epochs=config.NUM_EPOCHS, accelerator = config.ACCELERATOR, callbacks=[checkpoint_callback],
+    trainer = Trainer(min_epochs=0, max_epochs= params["training_parameter"]["num_epochs"], accelerator = params["compute"]["accelerator"], callbacks=[checkpoint_callback],
                         logger = mlf_logger, log_every_n_steps = 5)
 
     trainer.fit(model,dm)
